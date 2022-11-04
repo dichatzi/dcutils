677ff2cc66004e7c6f892fd1036b2cb251236033
dichatzi <61872599+dichatzi@users.noreply.github.com>
2022-11-04 14:39:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = "0.0.1"
+DESCRIPTION = "Utilities developed by Dr. Dimitris I. Chatzigiannis"
+LONG_DESCRIPTION = "Various utilitites related to database connection, printing files, etc that are continuously used"
 
 setup(
     name="dcutils",
-    version="0.0.1",
-    description="Dimitris Chatzigiannis' private package for connecting to database",
-    url="",
+    version=VERSION,
+    author="Dr. Dimitris I. Chatzigiannis",
     author_email="dimitris.chatzigiannis@gmail.com",
-    license="unlicense",
-    packages=["dcutils"],
-    zip_safe=False
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,    
+    packages=find_packages(),
+    install_requires=["psycopg2", "pandas", "sqlalchemy"],
+    keywords=["utilities", "database_connection"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ]
 )
