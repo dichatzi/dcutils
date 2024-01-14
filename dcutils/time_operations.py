ba375e70100019f1fe4164842b691e2d553fa74d
Dimitris Chatzigiannis <dimitris.chatzigiannis@gmail.com>
2024-01-14 21:26:51 +0200

    Time operations module is still not working corectly

--- dcutils/time_operations.py
+++ dcutils/time_operations.py
@@ -2,24 +2,31 @@ import datetime
 import pytz
 
 
-class TimeObj:
+class TimezoneConverter:
     def __init__(self, date_string, date_string_format, timezone):
         self.date_string = date_string
         self.date_string_format = date_string_format
         self.timezone = pytz.timezone(timezone)
         self.date = self.timezone.localize(datetime.datetime.strptime(self.date_string, self.date_string_format))
 
+        # Initialize data for transformed timezone
+        self.transformed_timezone = self.timezone
+        self.transformed_date = self.date
+
     def transform_timezone(self, target_timezone):
 
         # Define new timezone
-        new_timezone = pytz.timezone(target_timezone)
+        self.transformed_timezone = pytz.timezone(target_timezone)
 
         # Change timezone
-        self.date = self.date.astimezone(new_timezone)
+        self.transformed_date = self.date.astimezone(self.transformed_timezone)
 
         return self
     
     def __call__(self):
         print(f"Date object time: {self.date}")
         print(f"Date object timezone: {self.date.tzinfo}")
+        print("*")
+        print(f"Transformed date object time: {self.transformed_date}")
+        print(f"Transformed date object timezone: {self.transformed_date.tzinfo}")
 
