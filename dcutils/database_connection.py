d0df432a718d104abb778cba201d2d9805e88612
Dimitris Chatzigiannis <dimitris.chatzigiannis@gmail.com>
2024-05-29 16:51:24 +0300

    Adding the PostgresData class

--- dcutils/database_connection.py
+++ dcutils/database_connection.py
@@ -28,6 +28,104 @@ class DatabaseObject:
         self.connection = self.engine.connect()
 
 
+class PostgresData:
+    def __init__(self, database_object, dataframe, table_name, on_fields):
+        self.database_object = database_object
+        self.dataframe = dataframe
+        self.database_name = table_name.split(".")[0]
+        self.schema_name = table_name.split(".")[1]
+        self.table_name = table_name.split(".")[2]
+        self.on_fields = on_fields
+        self.upd_fields = [field for field in self.dataframe.columns if field not in self.on_fields]
+
+
+    def upsert_table(self, truncate_table:bool=False, slice_length:int=0, sql_query:str=""):
+
+        """
+        INITIALIZE DATA
+        """
+        # Truncate table if flag is activated
+        if truncate_table:
+            # Create truncate table query
+            truncate_table_query = sqlalchemy.text(f"TRUNCATE TABLE {self.database_name}.{self.schema_name}.{self.table_name}")
+            self.database_object.connection.execute(truncate_table_query)
+
+            # Commit changes
+            self.database_object.connection.commit()
+
+        # Execute custom sql query
+        if len(sql_query) > 0:
+            # Create custom sql query
+            custom_sql_query = sqlalchemy.text(sql_query)
+            self.database_object.connection.execute(custom_sql_query)
+
+            # Commit changes
+            self.database_object.connection.commit()
+
+
+        # Calculate on fields string
+        on_field = ""
+        for on_id, field in enumerate(self.on_fields):
+            prefix = ", " if on_id > 0 else ""
+            on_field = f'{on_field}{prefix}"{field}"'
+
+        # Calculate update fields string
+        upd_field = ""
+        for upd_id, field in enumerate(self.upd_fields):
+            prefix = ", " if upd_id > 0 else ""
+            upd_field = f'{upd_field}{prefix}"{field}"'
+
+        # Calculate fields string
+        fields = f"{on_field}, {upd_field}"
+
+        # Calculate updates string
+        updates = ""
+        for id, field in enumerate(self.upd_fields):
+            prefix = ", " if id > 0 else ""
+            updates = f'{updates}{prefix}"{field}" = EXCLUDED."{field}"'
+
+
+        """
+        PERFORM UPSERT OPERATIONS
+        """
+        # Calculate the length of the examined slice
+        slice = self.dataframe.shape[0] if slice_length==0 else slice_length
+
+        # Find the number of iterations
+        no_iterations = math.ceil(self.dataframe.shape[0] / slice)
+
+        # Update table data for each iteration
+        for i in range(no_iterations):
+            # Calculate start and end rows
+            start_row = i * slice
+            last_row = min(start_row + slice, len(self.dataframe))
+
+            # Create sub dataframe
+            table_data = self.dataframe[start_row:last_row]
+
+            # Uploading data
+            if slice_length > 0:
+                print(f"Slice {i + 1}: updating data")
+
+
+
+            # > After that it was working before --------------------------
+            # Insert dataframe data to temporary table
+            table_data.to_sql("temp_table", self.database_object.engine, index=False, if_exists='replace')
+
+            # Upsert data from temporary table to database
+            upsert_table_query = sqlalchemy.text(f"INSERT INTO {self.database_name}.{self.schema_name}.{self.table_name} ({fields}) SELECT {fields} FROM temp_table WHERE true ON CONFLICT ({on_field}) DO UPDATE SET {updates}")
+            self.database_object.connection.execute(upsert_table_query)
+            
+            # Drop temporary table
+            drop_temporary_table_query = sqlalchemy.text("DROP TABLE IF EXISTS public.temp_table")
+            self.database_object.connection.execute(drop_temporary_table_query)
+
+            # Commit changes
+            self.database_object.connection.commit()
+
+
+
 class PgDatabaseData:
     def __init__(self, database_object, dataframe, database_name, schema_name, table_name, on_fields, upd_fields):
         self.database_object = database_object
@@ -138,15 +236,8 @@ def connect_to_mongo():
     
     # Connect to client
     client = pymongo.MongoClient(MONGO_CONNECTION_STRING)
-    
-    # Connect to database
-    energy_management = client["energy_management"]
-    
-    # Connect to collection
-    volue_forecasts = energy_management["volue_forecasts"]
-    
-    # Upsert data to database
-    result = upsert_mongo(forecast_data, ["trading_date", "update_date", "bidding_area"], volue_forecasts)
+
+    return client
 
 
 def upsert_mongo(items, filters, collection):
